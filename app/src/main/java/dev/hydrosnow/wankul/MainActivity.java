0c312550cdf1a34b535b41155df7327090e87535
Alexis Trupin <link9745@gmail.com>
2020-01-17 11:36:09 +0100

    doing nothing

--- app/src/main/java/dev/hydrosnow/wankul/MainActivity.java
+++ app/src/main/java/dev/hydrosnow/wankul/MainActivity.java
@@ -1,10 +1,10 @@
 package dev.hydrosnow.wankul;
-
+        
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
