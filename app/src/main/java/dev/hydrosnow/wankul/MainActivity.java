21b6b747cf43e1c36374f1da3e3f3f9faf99e56c
Alexis Trupin <link9745@gmail.com>
2020-04-21 13:36:25 +0200

    useless commit

--- app/src/main/java/dev/hydrosnow/wankul/MainActivity.java
+++ app/src/main/java/dev/hydrosnow/wankul/MainActivity.java
@@ -1,6 +1,5 @@
 package dev.hydrosnow.wankul;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -14,7 +13,6 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
