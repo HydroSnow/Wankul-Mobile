3f77911de36e827189396153a4110266756a9466
Alexis Trupin <link9745@gmail.com>
2020-05-03 23:36:31 +0200

    modifying versions, adding default activity

---
+++ app/src/main/java/dev/hydrosnow/wankul/activities/MainActivity.java
@@ -0,0 +1,14 @@
+package dev.hydrosnow.wankul.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import dev.hydrosnow.wankul.R;
+
+public class MainActivity extends AppCompatActivity {
+	@Override
+	protected void onCreate(final Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
